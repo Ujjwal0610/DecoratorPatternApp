7003f1aaaf6b9824f59c145fd23c7afc30452501
Ujjwal0610 <A00312260@mycambrian.ca>
2024-12-11 01:11:02 +0000

    Updated different classes

--- DecoratorPatternApp/Program.cs
+++ DecoratorPatternApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        IComponent<string> component = new ConcreteComponent();
+
+        IComponent<string> plainDecorator = new PlainDecorator(component);
+        IComponent<string> upperCaseDecorator = new UpperCaseDecorator(plainDecorator);
+        IComponent<string> colorDecorator = new ColorDecorator(upperCaseDecorator);
+
+        Console.WriteLine(component.GetText());
+        Console.WriteLine(plainDecorator.GetText());
+        Console.WriteLine(upperCaseDecorator.GetText());
+        Console.WriteLine(colorDecorator.GetText());
+    }
+}
